cec0c3fce52933a72d9600c90ac06ab408b93614
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-03 21:01:09 -0700

    Define methods in Students Controller and creates root, new and show (single student) views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,39 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.create(params.require(:student).permit(:name, :age))
+    if @student.valid?
+        redirect_to students_path
+    else
+        flash[:errors] = @student.errors.full_messages
+        redirect_to new_student_path
+    end
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student).permit(:name, :age))
+    redirect_to student_path(@student)
   end
 
   def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
   end
 end
