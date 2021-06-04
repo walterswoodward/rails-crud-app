34b036664d0b6c1796765f1af3c55ee68032a43d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-03 19:15:34 -0700

    Revise default routes added by the controller cli command - use resources routing shorthand instead

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'enrollments/index'
-  get 'enrollments/new'
-  get 'enrollments/create'
-  get 'enrollments/destroy'
-  get 'courses/index'
-  get 'students/index'
-  get 'students/new'
-  get 'students/create'
-  get 'students/show'
-  get 'students/edit'
-  get 'students/update'
-  get 'students/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # see https://guides.rubyonrails.org/routing.html
+  # A single call to resources can declare all of the necessary
+  # routes for your index, show, new, edit, create, update, and
+  # destroy actions.
+  resources :students
+  resources :enrollments, only: [:new, :create, :destroy]
 end
