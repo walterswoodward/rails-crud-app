7a1c32b9307b40741b7edb8638e32e8911dc2541
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-03 19:03:36 -0700

    rails g controller students index new create show edit update destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'students/index'
+  get 'students/new'
+  get 'students/create'
+  get 'students/show'
+  get 'students/edit'
+  get 'students/update'
+  get 'students/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
