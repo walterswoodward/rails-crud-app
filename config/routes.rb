d18f10fddb1aa9ff63028b0cb5c16c83c61f2dc0
walterswoodward <walter.sekope.woodward@gmail.com>
2021-06-03 19:16:52 -0700

    Adds missing comment line in routes.rb file from previous commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # see https://guides.rubyonrails.org/routing.html
-  # A single call to resources can declare all of the necessary
+  # A single call to resources can declare all of the negit statcessary
   # routes for your index, show, new, edit, create, update, and
   # destroy actions.
   resources :students
